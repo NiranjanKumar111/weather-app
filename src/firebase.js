d32f5cd037fb2f81c5c521cf035206adafead01e
Niranjan Kumar <ns04001@gmail.com>
2022-10-13 02:18:55 +0530

    weather-app-finished

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAruvHHTrGRtmYPsC0VmZo32-HPdO0DxlQ",
+  authDomain: "weather-app-be83d.firebaseapp.com",
+  projectId: "weather-app-be83d",
+  storageBucket: "weather-app-be83d.appspot.com",
+  messagingSenderId: "656428202698",
+  appId: "1:656428202698:web:89dbe88775f26ebceb1233",
+};
